f43821cda35f06016818f47855c24ccee313644c
ShadowFurtive <silencioso4444@gmail.com>
2022-05-15 00:26:12 +0200

    eliminando archivos

--- persona/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import PersonaTemplate, Client, Personal, Entrenador
-
-admin.site.register(PersonaTemplate)
-admin.site.register(Client)
-admin.site.register(Entrenador)
-admin.site.register(Personal)
